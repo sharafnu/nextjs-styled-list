b79d6e62fad65df86d66829882cf75ea99656702
Sharaf <sharaf.innovazions@gmail.com>
2024-05-19 21:43:26 +0400

    Create next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,15 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    images: {
+        domains: [
+          'upload.wikimedia.org',
+          'www.freepnglogos.com',
+          'seeklogo.com',
+          'vecteezy.com',
+          'freepnglogos.com',
+        ],
+      },
+  };
+  
+  export default nextConfig;
+  
